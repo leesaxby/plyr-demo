e19f9e3d054a53c8edab4618b0f99b17bf22cef3
leesaxby <lee.saxby82@gmail.com>
2022-01-27 18:30:32 +0000

    Remove class that fails lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-      <div class="plyr__video-embed" id="player">
+      <div id="player">
         <iframe
           src="https://www.youtube.com/embed/bTqVqk7FSmY?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
           allow="autoplay">
