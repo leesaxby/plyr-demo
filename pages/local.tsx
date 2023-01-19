8f50c2c6f3111b2aaba3e6662776e24d42a74cba
leesaxby <lee.saxby82@gmail.com>
2023-01-19 13:59:47 +0000

    Add query param

--- pages/local.tsx
+++ pages/local.tsx
@@ -1,14 +1,26 @@
-import type { NextPage } from 'next'
 import Layout from "../components/layout";
+import Link from "next/link";
 
-const Local: NextPage = () => {
+const Local: any = ({ name }: { name: string} ) => {
     return (
         <Layout>
+            <h1>{name}</h1>
             <video id="player" playsInline controls>
                 <source src="/video/local-video.mp4" type="video/mp4"/>
             </video>
+            <Link href="/url">
+                <a>URL SOURCE (with poster image)</a>
+            </Link>
         </Layout>
     )
 }
 
 export default Local
+
+export async function getServerSideProps(context: any) {
+    return {
+        props: {
+            name: context.query.tester
+        },
+    }
+}
